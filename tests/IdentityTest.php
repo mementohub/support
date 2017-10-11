8288bfb20d1eac9d2561e8f919543a364642c976
Andrei Penciu <andrei.penciu@mementohub.com>
2017-10-11 18:11:35 +0300

    add helpers tests

--- tests/IdentityTest.php
+++ tests/IdentityTest.php
@@ -9,14 +9,18 @@ class IdentityTest extends \PHPUnit\Framework\TestCase
     /** @test */
     public function encode_uses_glue_to_transform_user_id_agency_id_into_identity()
     {
+        $actual = [[1, 3], [2, null], [3]];
         $expected = ['1.3', '2.', '3.'];
-        $this->assertEquals($expected, Identity::encode([[1, 3], [2, null], [3]]));
+        $this->assertEquals($expected, Identity::encode($actual));
+        $this->assertEquals($expected, identity_encode($actual));
     }
 
     /** @test */
     public function decode_uses_glue_to_transform_identity_into_user_id_agency_id()
     {
+        $actual = ['1.3', '2.'];
         $expected = [[1, 3], [2, null]];
-        $this->assertEquals($expected, Identity::decode(['1.3', '2.']));
+        $this->assertEquals($expected, Identity::decode($actual));
+        $this->assertEquals($expected, identity_decode($actual));
     }
 }
