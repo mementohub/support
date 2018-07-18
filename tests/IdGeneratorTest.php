cdff4108b808af7d49f659df50d32546291fa85d
George Stan <contact@georgestan.com>
2018-07-18 19:20:30 +0300

    tests

---
+++ tests/IdGeneratorTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace iMemento\Support\Tests;
+
+use iMemento\Support\IdGenerator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers Client
+ */
+class IdGeneratorTest extends TestCase
+{
+
+    public function test_id_length_is_correct()
+    {
+        for ($i = 0; $i < 5; $i++) {
+            $length = mt_rand(1, 32);
+
+            $id = IdGenerator::generate($length);
+
+            $this->assertEquals($length, strlen($id));
+        }
+    }
+
+}
